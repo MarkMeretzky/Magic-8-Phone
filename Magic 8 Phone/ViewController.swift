7b447f2fb14d3cb92d988cbd0e2445c4f605e332
Jeffrey <Jeffrey@simpleyapp.com>
2020-04-08 16:22:38 -0400

    Magic 8 Phone

--- Magic 8 Phone/ViewController.swift
+++ Magic 8 Phone/ViewController.swift
@@ -10,11 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var answerTextView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    let answers : [String] = ["As I see it, yes.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","It is certain.","It is decidedly so."]
+    
+    @IBAction func swipeRight(_ sender: Any) {
+        
+        let number: Int = Int.random(in: 0 ..< 7)
+        
+        answerTextView.text = answers[number]
+        
+    }
+    
+    @IBAction func swipeLeft(_ sender: Any) {
+        
+        let number: Int = Int.random(in: 0 ..< 7)
+        
+        answerTextView.text = answers[number]
+        
+    }
+    
 
 }
 
