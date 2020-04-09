ae8a22293e485558d4894790a3584df45b39cdc5
Mark Meretzky <mark.meretzky@gmail.com>
2020-04-08 20:29:29 -0400

    Made two lists of answers, not just one.  And just one action, with an if statement.

--- Magic 8 Phone/ViewController.swift
+++ Magic 8 Phone/ViewController.swift
@@ -13,31 +13,45 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var answerTextView: UITextView!
     
+    let leftAnswers: [String] = [
+        "As I see it, yes.",
+        "Ask again later.",
+        "Better not tell you now.",
+        "Cannot predict now.",
+        "Concentrate and ask again.",
+        "Don't count on it.",
+        "It is certain.",
+        "It is decidedly so.",
+        "Ask me when you have a present for me"
+    ];
+    
+    let rightAnswers: [String] = [
+        "No taxes!",
+        "No government!",
+        "Big military!",
+        "No givernment oversight of Wall Street!"
+    ]
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    
-    let answers : [String] = ["As I see it, yes.","Ask again later.","Better not tell you now.","Cannot predict now.","Concentrate and ask again.","Don't count on it.","It is certain.","It is decidedly so."]
-    
-    @IBAction func swipeRight(_ sender: Any) {
-        
-        let number: Int = Int.random(in: 0 ..< 7)
-        
-        answerTextView.text = answers[number]
-        
-    }
-    
-    @IBAction func swipeLeft(_ sender: Any) {
-        
-        let number: Int = Int.random(in: 0 ..< 7)
-        
-        answerTextView.text = answers[number]
-        
-    }
-    
 
+    @IBAction func swipeRight(_ sender: UISwipeGestureRecognizer) {
+        print(sender.direction);
+        
+        
+        if sender.direction == .left {
+            let number: Int = Int.random(in: 0 ..< leftAnswers.count)
+            answerTextView.text = leftAnswers[number];
+        } else {
+            let number: Int = Int.random(in: 0 ..< rightAnswers.count)
+            answerTextView.text = rightAnswers[number];
+        }
+            
+        //answerTextView.text = answers[number]
+        
+    }
 }
-
